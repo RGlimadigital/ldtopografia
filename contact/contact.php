4e61cbbf3ea7e9b897a264496564ebc2462153d4
RgFullDev <rgfulldev@gmail.com>
2020-06-06 10:30:07 +0200

    Salvando modelo-01 , novo Quem somos.

---
+++ contact/contact.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
+*/
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require './PHPMailer-master/vendor/autoload.php';
+
+/*
+*  CONFIGURE EVERYTHING HERE
+*/
+
+// an email address that will be in the From field of the email.
+$fromEmail = $_POST['email'];
+$fromName = $_POST['name'];
+
+// an email address & name that will receive the email with the output of the form
+$sendToEmail = 'info@ldtopografia.com.br';
+// $sendToName = 'LD Topografia'; 
+
+// subject of the email
+$subject = 'Formulário de contato.';
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name:', 'email' => 'Email:', 'message' => 'Message:');
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Formulário enviado com Sucesso - Em breve estaremos respondendo!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'Sua mensagem não pode ser enviada. Em alguns momentos tente novamente.';
+
+/*
+*  LET'S DO THE SENDING
+*/
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+    
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+    $emailTextHtml .= "<h3>Mensagem - Formulário de Contato - LDTopografia.com.br</h3><hr>";
+    $emailTextHtml .= "<table>";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email
+        if (isset($fields[$key])) {
+            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+        }
+    }
+    $emailTextHtml .= "</table><hr>";
+    $emailTextHtml .= "<p>Muito Obrigado por sua mensagem. <br><br><br><br>LD Topogra</p>";
+    
+    $mail = new PHPMailer;
+
+    $mail->setFrom($fromEmail, $fromName);
+    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+    
+
+
+    $mail->Subject = $subject;
+
+    $mail->Body = $emailTextHtml;
+    $mail->isHTML(true);
+    //$mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
+    
+    
+    if(!$mail->send()) {
+        throw new \Exception('Email send failed. ' . $mail->ErrorInfo);
+    }
+    
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+}
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
+?>
